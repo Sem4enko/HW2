b3e3f5b521243c9fd0547c08b3f1448870c1d10b
Sem4enko <74368102+Sem4enko@users.noreply.github.com>
2020-11-19 15:20:19 +0200

    Update Task3.java

--- src/homework2/Task3.java
+++ src/homework2/Task3.java
@@ -2,14 +2,13 @@ package homework2;
 
 public class Task3 {
     public static void main(String[] args) {
-        double[] a = {10, 2, 3, 4, 5};
+        int[] a = {10, 2, 3, 4, 5};
         int sum = 0;
         double avg = 0;
         for (int i = 0; i < a.length; i++) {
             sum += a[i];
-            avg = sum / a[i];
+            avg = (double) sum / a.length;
         }
-
         System.out.println(avg);
     }
 }
